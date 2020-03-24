9fec6f36e935a5844829ac09bb7a40f2b562fe34
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-24 21:23:02 +0200

    Add project files.

---
+++ SoccerTeamsRanking.Tests/ProgramsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace SoccerTeamsRanking.Tests
+{
+    public class ProgramsTests
+    {
+        [Fact]
+        public void BubbleSortsWorksWithSimpleArray()
+        {
+            SoccerTeam[] teams = { new SoccerTeam("FCSB", 33), new SoccerTeam("Sepsi OSK ", 28), new SoccerTeam("CS Gaz Metan Medias", 29) };
+            SoccerTeam[] teamsSorted = { new SoccerTeam("FCSB", 33),  new SoccerTeam("CS Gaz Metan Medias", 29), new SoccerTeam("Sepsi OSK ", 28) };
+            Program.BubbleSort(teams);
+            Assert.Equal<SoccerTeam>(teams, teamsSorted);
+
+        }
+    }
+}
